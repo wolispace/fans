c3e6365de95a102ca2e23658df74ad4a87191a53
wolispace <wm@wolispace.com>
2018-06-12 13:50:59 +0930

    start on UI mockup with basic functionality of some lights
    Bedroom light only

--- server.js
+++ server.js
@@ -18,16 +18,39 @@ console.log("server is on " + settings.port);
 // handle requests..
 function handler (req, res) {
   // no need for url passing - just look for css or html and show that otherwise control a fanx@ 
-  var stuff = req.url.substring(1);
-  console.log(stuff);
+  var stuff = req.url.substring(1).replace('+', ' ');
+  console.log('[' + stuff + ']');
   if (stuff === '') {
+    console.log('sending index.html');
     serverStaticFile('index.html', res);
+  } else if (stuff.indexOf('favicon') > -1) {
+    // do nothing..
+    console.log('doing nothing');
+  } else {
+    console.log(myFans);
+    var params = extractUrlParams(stuff);
+    console.log('processing ', params);
+    //console.log('fans', myFans);
+
+    var fanName = params['device'];
+    var oneFan = myFans[fanName];
+    console.log('Controlling ' + fanName + ' ', params);
+    if (oneFan) {
+      console.log(fanName + ' found. brightness was [' + oneFan.light.brightness +']');
+      if (params.brightness) {
+        oneFan.light.brightness = params.brightness;
+        console.log(fanName + ' found. brightness is now [' + oneFan.light.brightness +']');
+      }
+    }
+    console.log('Giving browser', params);
+    res.writeHead(200, "application/json");
+    res.end(JSON.stringify(params));
   }
-  //console.log('myFans', myFans);
 }
 
 function serverStaticFile (fileName, res) {
   fileName = (fileName === '/') ? '/index.html' : fileName;
+  console.log('sending file '+ settings.root+fileName);
   fs.readFile(settings.root+fileName,
     function (err, data) {
       if (err) {
@@ -39,4 +62,18 @@ function serverStaticFile (fileName, res) {
       }
     }
   );
-}
+};
+
+// splits out params from ?bit=1&bob=thing  = {bit:1, bob='thing'}
+function extractUrlParams (req) {
+  // remove the leading ?
+  var stuff = req.substring(1);
+  var bits = stuff.split('&');
+  var params = {};
+  bits.forEach( function (param, index) {
+    var parts = param.split('=');
+    params[parts[0]] = parts[1]; 
+  });
+
+  return params;
+};
